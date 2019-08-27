7c3762c040a81e8cb1e8d3f8aa8b31cee3e20640
uday-git-lab <udayakmr105@gmail.com>
2019-08-27 10:30:24 +0000

    added-style

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -11,9 +11,11 @@ public class Greeter {
   public Greeter() {
 
   }
-
-  //TODO: Add javadoc comment
-  public String greet( final String someone) {
+  /**
+ *    * This is a string block
+ */
+  
+public String greet( final String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
