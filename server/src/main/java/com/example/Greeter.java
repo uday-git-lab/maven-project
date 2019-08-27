25b9ed89883506f7ba59332b0181b1848ed05a0c
uday-git-lab <udayakmr105@gmail.com>
2019-08-27 12:31:59 +0000

    style

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,8 @@ public class Greeter {
 
   }
   /**
- *    * This is a string block
- */
+  * This is a string block
+  */
   
 public String greet( final String someone) {
     return String.format("Hello, %s!", someone);
